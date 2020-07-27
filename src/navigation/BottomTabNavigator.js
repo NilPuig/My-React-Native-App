a56302281a532dc5c2d85d9976fe97c5160ed08b
Nil <nil.puigm@gmail.com>
2020-07-27 18:30:54 +0800

    change bottom bar icons

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
 
 import HomeNavigator from './HomeNavigator';
 import SearchNavigator from './SearchNavigator';
@@ -7,9 +9,25 @@ import SearchNavigator from './SearchNavigator';
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Home" component={HomeNavigator} />
-    <Tab.Screen name="Search" component={SearchNavigator} />
+  <Tab.Navigator tabBarOptions={{ activeTintColor: 'black' }}>
+    <Tab.Screen
+      name="Home"
+      component={HomeNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <Ionicons name="home" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Search"
+      component={SearchNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <Feather name="search" color={color} size={size} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
